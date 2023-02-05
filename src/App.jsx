03c3cb05a1b3ff3c0d441a62e89440ba24862fd1
betting-iscringe <capthancock@protonmail.com>
2023-02-05 23:14:03 +0800

    Revert "Upgraded antd"
    This reverts commit 801ba985e37d19c6f1611b518c6626b6778a803c.

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ const defaultEvent = "hfz";
 
 export default function App() {
   const [event, setEvent] = useState([defaultEvent]);
-  const [treeOpen, setTreeOpen] = useState(false);
+  const [treeVisible, setTreeVisible] = useState(false);
   const [usernameFilter, setUsernameFilter] = useState("");
   const [checkedKeys, setCheckedKeys] = useState([]);
   const [dataHolder, setDataHolder] = useState({});
@@ -86,7 +86,7 @@ export default function App() {
     setLoading(false);
   };
 
-  const getData = async (events, resetOpen = true) => {
+  const getData = async (events, resetVisible = true) => {
     let usedEvents = [];
     let keysHolder = [];
     let dataHolder = [];
@@ -139,7 +139,7 @@ export default function App() {
     setDataHolder(totalData);
     setEventHolder(usedEvents);
     setTreeData(dataHolder);
-    if (resetOpen) {
+    if (resetVisible) {
       setCheckedKeys(keysHolder.filter((key) => !key.startsWith("EVENT_")));
     } else {
       setCheckedKeys(checkedKeys);
@@ -318,8 +318,8 @@ export default function App() {
                 />
               }
               trigger={["click"]}
-              open={treeOpen}
-              onOpenChange={setTreeOpen}
+              visible={treeVisible}
+              onVisibleChange={setTreeVisible}
               onContextMenu={onRightClick}
             >
               <FilterFilled
